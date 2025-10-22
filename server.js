931f5892e636daba71070694d3d352f362612681
KishorKumar <kishor.r.ihub@snsgroups.com>
2025-10-22 22:13:41 +0530

    allowed cors to access from any where

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: true,  // allow requests from any origin
   credentials: true
 }));
 
